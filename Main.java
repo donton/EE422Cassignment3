3455a2ee80732b5ed7677d4e07193a5a60c1e201
Don Ton <donton@utexas.edu>
2016-09-27 15:09:22 -0500

    Added my name

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
+ * Don Ton
+ * dt22776
  * <Student1 5-digit Unique No.>
  * <Student2 Name>
  * <Student2 EID>
