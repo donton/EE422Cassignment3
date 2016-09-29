82ad2471b3f3bd9529133880a200015318df9b76
donton <donton@utexas.edu>
2016-09-28 19:20:20 -0500

    Add files via upload

--- Node.java
+++ Node.java
@@ -1,5 +1,21 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Manuel Gomez	
+ * mlg3454
+ * 16475
+ * Don Ton
+ * dt22776
+ * 16470
+ * Slip days used: <0>
+ * Git URL: https://github.com/donton/EE422Cassignment3
+ * Fall 2016
+ */
+
 package assignment3;
 
+/**
+ * Node --- Contains nodes' information.
+ */
 public class Node {
 	String word;
 	Node parent;
